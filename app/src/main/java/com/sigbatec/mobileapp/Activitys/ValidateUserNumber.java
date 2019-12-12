448de8f8a4f79eeeff261eb59d8f89b71937af77
giovannibenvenuto <benvenuto.acevedo@gmail.com>
2019-12-12 20:41:13 -0300

    subiendo todo lo que faltaba :)

---
+++ app/src/main/java/com/sigbatec/mobileapp/Activitys/ValidateUserNumber.java
@@ -0,0 +1,16 @@
+package com.sigbatec.mobileapp.Activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sigbatec.mobileapp.R;
+
+public class ValidateUserNumber extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validate_user_number);
+    }
+}
--- app/src/main/java/com/sigbatec/mobileapp/MainActivity.java
+++ app/src/main/java/com/sigbatec/mobileapp/MainActivity.java
@@ -6,8 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.sigbatec.mobileapp.Activitys.ValidateUserNumber;
+import com.sigbatec.mobileapp.model.ResObj;
+import com.sigbatec.mobileapp.remote.ApiUtils;
+import com.sigbatec.mobileapp.remote.UserService;
+
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView Registar;
+    EditText edtUsername;
+    EditText edtPassword;
+    Button btnLogin;
+    UserService userService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,57 +32,87 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        edtUsername = (EditText) findViewById(R.id.Usuario);
+        edtPassword = (EditText) findViewById(R.id.Password);
+        btnLogin = (Button) findViewById(R.id.Inicio);
+        Registar = (TextView) findViewById(R.id.Registro);
+        userService = ApiUtils.getUserService();
+
         Button siguiente;
 
-        siguiente = (Button) findViewById(R.id.buttonEnt);
+        siguiente = (Button) findViewById(R.id.Inicio);
         siguiente.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent siguiente = new Intent(MainActivity.this, InicioApp.class);
-                startActivity(siguiente);
-
+                String username = edtUsername.getText().toString();
+                String password = edtPassword.getText().toString();
+                //validate form
+                if (validateLogin(username, password)) {
+                    //do login
+                    doLogin(username, password);
+                }
 
 
             }
         });
-        /*
 
-        @Override
-        protected void onCreate (Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main2);
 
-            Recupe siguiente2;
+        Registar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ValidateUserNumber.class);
+                startActivity(intent);
 
-            siguiente2 = (Recupe) findViewById(R.id.Recuperar);
-            siguiente2.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                    Intent i = new Intent(MainActivity.this, Main3ActivityR.class);
-                    startActivity(i);
+            }
+        });
+
+
+    }
+
+    private void doLogin(final String username, final String password) {
+
+        retrofit2.Call<ResObj> call = userService.login(username, password);
+        call.enqueue(new Callback<ResObj>() {
+            @Override
+            public void onResponse(retrofit2.Call<ResObj> call, Response<ResObj> response) {
+                if (response.isSuccessful()) {
+                    ResObj ResObj = (com.sigbatec.mobileapp.model.ResObj) response.body();
+                    if (ResObj.getMessage().equals("true")) {
+                        //login start main activity
+                        Intent intent = new Intent(MainActivity.this, InicioApp.class);
+                        intent.putExtra("username", username);
+                        startActivity(intent);
+
+                    } else {
+                        Toast.makeText(MainActivity.this, "El usuario o Contraseña es Incorrecto", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Error! Favor Reintentar", Toast.LENGTH_SHORT).show();
                 }
-            });
+            }
 
+            @Override
+            public void onFailure(retrofit2.Call<ResObj> call, Throwable t) {
+
+                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    private boolean validateLogin(String username, String password) {
+
+        if (username == null || username.trim().length() == 0) {
+            Toast.makeText(this, "Email es requerido", Toast.LENGTH_SHORT).show();
+            return false;
         }
+        if (password == null || password.trim().length() == 0) {
+            Toast.makeText(this, "Password es requerido", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-
-            Button siguiente3;
-
-            siguiente3 = (Button) findViewById(R.id.button7);
-            siguiente3.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                    Intent siguiente3 = new Intent(MainActivity.this, MainActivity4.class);
-                    startActivity(siguiente3);
-
-                }
-            });
 
     }
 
-         */
-
-    }
 }
