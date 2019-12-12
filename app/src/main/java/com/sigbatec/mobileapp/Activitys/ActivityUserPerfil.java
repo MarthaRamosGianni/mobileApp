cedd279a60a9f2c29937321b3189aacfe761cc2e
giovannibenvenuto <benvenuto.acevedo@gmail.com>
2019-12-12 19:20:04 -0300

    subir faltantes proyecto martha

---
+++ app/src/main/java/com/sigbatec/mobileapp/Activitys/ActivityUserPerfil.java
@@ -0,0 +1,16 @@
+package com.sigbatec.mobileapp.Activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sigbatec.mobileapp.R;
+
+public class ActivityUserPerfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_perfil);
+    }
+}
